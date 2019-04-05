fb4a729c2f366f701d3447b9297a7901ce6bb162
Daniel Tymecki <daniel.tymecki@gmail.com>
2019-04-05 15:02:11 -0400

    400 IQ Ocaml Sort Play
    Sort works now lol

--- poly.ml
+++ poly.ml
@@ -197,7 +197,12 @@ let equal_pExp (_e1: pExp) (_e2: pExp) : bool =
     | Term(c1, v1), Term(c2,v2) -> _e1 = _e2
     | Plus(plist1), Plus(plist2) -> Plus(List.sort compare plist1) = Plus(List.sort compare plist2)
     | Times(plist1), Times(plist2) -> Times(List.sort compare plist1) = Times(List.sort compare plist2)
-    | _ -> false)
+    | _ -> false);;
+
+let rec sort (_s: pExp): pExp = match _s with
+  | Term(c,v) -> _s;
+  | Plus(plist) -> Plus(List.sort compare (List.map sort plist))
+  | Times(plist) -> Times(List.sort compare (List.map sort plist));;
 
 (* Fixed point version of simplify1 
   i.e. Apply simplify1 until no 
